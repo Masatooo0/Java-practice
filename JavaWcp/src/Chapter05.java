a9ded58ae321d6fbc577c92e96d09e53f3001da6
Masatooo0 <masato.n.0321@gmail.com>
2021-08-18 14:48:52 +0900

    [add]Chapter05

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,23 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 100000L;
+		double d = 3.14;
+		boolean b = true;
+		
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(b);
+		
+		Float f = 1.234F;
+		Integer i = 5;
+		Character c = 'あ';
+		System.out.println(f);
+		System.out.println(i);
+		System.out.println(c);
+		
+		int[] array = {1,2,3,4,5};
+		System.out.println(array[3]);
+	}
+	
+}
