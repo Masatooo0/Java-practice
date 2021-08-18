95c812a95c49fd82e75a1a4b15318b8a32da80c7
Masatooo0 <masato.n.0321@gmail.com>
2021-08-18 15:21:50 +0900

    [add]Switch01

--- JavaWcp/src/if02.java
+++ JavaWcp/src/if02.java
@@ -1,7 +1,7 @@
 
 public class if02 {
 	public static void main(String[] args) {
-		int number = 15;
+		int number = 7;
 		if(number < 5) {
 			System.out.println(number + "は5未満です");
 		}
@@ -12,7 +12,7 @@ public class if02 {
 			System.out.println(number + "10以上15未満です");
 		}
 		else {
-			System.out.println(number + "15未満でないです");
+			System.out.println(number + "は15未満でないです");
 		}
 	}
 }
